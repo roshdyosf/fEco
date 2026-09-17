664d7228dbaf0a8886e1de7132e9718baeb3cc0d
roshdyosf <yosef2236303@gmail.com>
2026-09-17 07:57:47 +0300

    feat: update user_id foreign key to be nullable and use nullOnDelete for improved data handling

--- database/migrations/2026_09_14_160157_create_transactions_table.php
+++ database/migrations/2026_09_14_160157_create_transactions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('family_id')->constrained('families')->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
             $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
             $table->decimal('amount', 10, 2);
             $table->enum('type', ['expense', 'income']);
